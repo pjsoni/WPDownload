6f5d0142a47102a230752afe16e33f5bff6e33c4
Pravesh Soni <pjsoni@users.noreply.github.com>
2021-08-20 17:28:15 -0500

    Initial Commit

---
+++ WPDownload/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace WPDownload
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.numericUpDownIndex = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownCount = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownIndex)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownCount)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(12, 64);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(566, 453);
+            this.textBox1.TabIndex = 1;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // numericUpDownIndex
+            // 
+            this.numericUpDownIndex.Location = new System.Drawing.Point(60, 28);
+            this.numericUpDownIndex.Name = "numericUpDownIndex";
+            this.numericUpDownIndex.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDownIndex.TabIndex = 2;
+            this.numericUpDownIndex.ValueChanged += new System.EventHandler(this.numericUpDownIndex_ValueChanged);
+            // 
+            // numericUpDownCount
+            // 
+            this.numericUpDownCount.Location = new System.Drawing.Point(266, 28);
+            this.numericUpDownCount.Name = "numericUpDownCount";
+            this.numericUpDownCount.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDownCount.TabIndex = 3;
+            this.numericUpDownCount.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDownCount.ValueChanged += new System.EventHandler(this.numericUpDownCount_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Index";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(216, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Number";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(505, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Download";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(594, 529);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDownCount);
+            this.Controls.Add(this.numericUpDownIndex);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownIndex)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownCount)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.NumericUpDown numericUpDownIndex;
+        private System.Windows.Forms.NumericUpDown numericUpDownCount;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WPDownload/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.ServiceModel.Syndication;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace WPDownload
+{
+    public partial class Form1 : Form
+    {
+        private delegate void SetTextCallback(string text);
+        private string DOWNLOAD_PATH = string.Empty;
+        private string logFile = "wpdownload.txt";
+        private Stopwatch sw = new Stopwatch();
+        int imgIndex = 0;
+        int imgCount = 10;
+
+        private void SetText(string text)
+        {
+            // InvokeRequired required compares the thread ID of the calling thread to the thread ID
+            // of the creating thread. If these threads are different, it returns true.
+            if (this.textBox1.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.textBox1.AppendText(text);
+            }
+        }
+
+        private void SetDownloadPath() {
+            if (!string.IsNullOrEmpty(ConfigurationManager.AppSettings["downloadLocation"]) && ConfigurationManager.AppSettings["downloadLocation"].ToString().ToUpper() == "USERPROFILE")
+            {
+                DOWNLOAD_PATH = String.Format("{0}\\{1}", Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ConfigurationManager.AppSettings["downloadFolder"]);  
+
+            }
+            else {
+                DOWNLOAD_PATH =  ConfigurationManager.AppSettings["downloadFolder"];
+
+            }
+
+
+        }
+
+    public Form1()
+        {
+            InitializeComponent();
+            numericUpDownIndex.Value = imgIndex;
+            numericUpDownCount.Value = imgCount;
+            SetDownloadPath();
+           
+
+        }
+
+        
+
+        private void SaveImage(string url, string datesuffix)
+        {
+            string fileName = GetServerImageName(url);
+            string localFileName = GetLocalImageName(datesuffix);
+            try
+            {
+                WebRequest req = WebRequest.Create(@"http://bing.com" + url.Replace("1366x768", "1920x1200"));
+                using (var stream = req.GetResponse().GetResponseStream())
+                {
+                    Image img = Image.FromStream(stream);
+                    img.Save(localFileName);
+                    SetText(string.Format("Saved image - {0}", localFileName) + Environment.NewLine);
+                }
+                using (StreamWriter sw = File.AppendText(logFile))
+                {
+                    sw.WriteLine(url);
+                }
+            }
+            catch (WebException ex)
+            {
+                if (ex.Status == WebExceptionStatus.ProtocolError && ((HttpWebResponse)ex.Response).StatusCode == HttpStatusCode.NotFound)
+                {
+                    SetText(string.Format("File not found on server - {0}", fileName) + Environment.NewLine);
+
+                    // do nothing
+                }
+                else
+                {
+                    HandleException(ex);
+                }
+            }
+        }
+
+        private string GetLocalImageName(string datesuffix)
+        {
+            DateTime dt = DateTime.ParseExact(datesuffix, "yyyyMMdd", CultureInfo.InvariantCulture, DateTimeStyles.None).AddDays(-1);
+            return string.Format(@"{0}\BingWallpaper-{1}.jpg", DOWNLOAD_PATH, dt.ToString("yyyy-MM-dd"));
+        }
+
+        private string GetServerImageName(string url)
+        {
+            Regex reg = new Regex(@"[^/]*(?=\.jpg|JPG.*$)");
+            Match m = reg.Match(url);
+            var retVal = string.Format("{0}.jpg", m.Value);
+            return retVal.Replace("1366x768", "1920x1200");
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            #region Download from BING Commented Code
+
+            sw.Start();
+
+            XDocument doc = null;
+            HttpWebRequest request = null;
+
+           
+            string url = string.Format(ConfigurationManager.AppSettings["bingUrl"], imgIndex, imgCount);
+            try
+            {
+                request = (HttpWebRequest)WebRequest.Create(url);
+                using (var stream = request.GetResponse().GetResponseStream())
+                {
+                    doc = XDocument.Load(stream);
+                }
+
+               
+                SetText(String.Format("Total images found - {0}", doc.Descendants("image").Count().ToString()) + Environment.NewLine);
+
+                foreach (var image in doc.Descendants("image"))
+                {
+                    string fileName = GetLocalImageName(image.Element("enddate").Value);
+                    if (!File.Exists(fileName))
+                    {
+                        SaveImage(image.Element("url").Value, image.Element("enddate").Value);
+                    }
+                    else
+                    {
+                        SetText(string.Format("Local file exist - {0}", fileName) + Environment.NewLine);
+                    }
+                }
+
+               
+            }
+            catch (Exception ex)
+            {
+                HandleException(ex);
+            }
+
+            
+        }
+
+        private void HandleException(Exception ex)
+        {
+            //SetText("-------------------------" + Environment.NewLine);
+            SetText(ex.Message + Environment.NewLine);
+
+            //SetText("-------------------------" + Environment.NewLine);
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            sw.Stop();
+
+            SetText(String.Format("Elapsed={0}", sw.Elapsed) + Environment.NewLine);
+            SetText("Task completed" + Environment.NewLine);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void numericUpDownCount_ValueChanged(object sender, EventArgs e)
+        {
+            imgCount = Convert.ToInt32(numericUpDownCount.Value);
+        }
+
+        private void numericUpDownIndex_ValueChanged(object sender, EventArgs e)
+        {
+            imgIndex = Convert.ToInt32(numericUpDownIndex.Value);
+        }
+    }
+}
